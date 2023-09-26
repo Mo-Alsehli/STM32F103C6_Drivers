d462df7c3629d7d7645dcadb6b6755682f532c4b
Mo Alsehli <mohamed.m.alsehli@gmail.com>
2023-09-26 18:13:17 +0300

    small test

--- Src/main.c
+++ Src/main.c
@@ -8,7 +8,7 @@
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
 #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
-// Here after Pulling.
+// Here after Pulling and Then with a small change.
 #include "STM32F103x6.h"
 #include "GPIO_Driver.h"
 
