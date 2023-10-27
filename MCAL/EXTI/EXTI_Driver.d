f1f8c1c7f87085f12ab542ac8444e99249fdcbde
Mo Alsehli <mohamed.m.alsehli@gmail.com>
2023-10-27 16:21:14 +0200

    external interrupt driver added

---
+++ MCAL/EXTI/EXTI_Driver.d
@@ -0,0 +1,9 @@
+MCAL/EXTI/EXTI_Driver.o: ../MCAL/EXTI/EXTI_Driver.c \
+ ../MCAL/EXTI/EXTI_Driver.h ../Inc/STM32F103x6.h \
+ C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/MCAL/GPIO/GPIO_Driver.h
+
+../MCAL/EXTI/EXTI_Driver.h:
+
+../Inc/STM32F103x6.h:
+
+C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/MCAL/GPIO/GPIO_Driver.h:
---
+++ MCAL/EXTI/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../MCAL/EXTI/EXTI_Driver.c 
+
+OBJS += \
+./MCAL/EXTI/EXTI_Driver.o 
+
+C_DEPS += \
+./MCAL/EXTI/EXTI_Driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+MCAL/EXTI/EXTI_Driver.o: ../MCAL/EXTI/EXTI_Driver.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/HAL/Keypad" -I"C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/HAL/LCD" -I"C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/MCAL/GPIO" -I"C:/Users/dell/STM32CubeIDE/workspace_1.4.0/STM32F103C6_Drivers/MCAL/EXTI" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"MCAL/EXTI/EXTI_Driver.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
